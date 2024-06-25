1f7f0043863792fd3f1827e285e49dda4064e3a0
CaioSouzaC1 <caiocsouza2010@gmail.com>
2024-06-24 22:04:47 -0300

    <CHORE>[Projects]: Updating projects List

--- src/components/ProjectCard/index.tsx
+++ src/components/ProjectCard/index.tsx
@@ -3,18 +3,29 @@ interface IProjectCard {
   image: string;
   text: string;
   tec: string;
+  badge: string;
+  preview?: string;
   link?: string;
 }
 
-const ProjectCard = ({ title, image, text, tec, link }: IProjectCard) => {
+interface IInfoCard {
+  info: IProjectCard;
+}
+
+const ProjectCard = ({ info }: IInfoCard) => {
+  const { image, title, text, tec, badge, preview, link } = info;
+
   return (
-    <div className="w-full md:w-1/2 lg:w-1/3 text-center px-4 mb-20 lg:mb-8">
+    <div className="w-full md:w-1/2 lg:w-1/3 text-center px-4 mb-20 lg:mb-8 relative">
       <img
-        className="w-full max-h-48 object-cover rounded mb-4"
+        className="w-full h-40 object-cover rounded mb-4"
         src={image}
         alt={title}
       />
-      <div className="min-h-48">
+      <div className="inline-flex items-center rounded-md border px-2.5 py-0.5 text-sm font-semibold absolute top-4 right-8 bg-black/80 hover:bg-black/80">
+        {badge}
+      </div>
+      <div className="min-h-48 mb-8">
         <p className="font-bold mb-4 border-b border-b-sky-500 text-2xl inline-block">
           {title}
         </p>
@@ -24,15 +35,27 @@ const ProjectCard = ({ title, image, text, tec, link }: IProjectCard) => {
           ).
         </p>
       </div>
-      {link && (
-        <a
-          className="px-8 py-4 bg-gradient-to-r from-stone-800 to-stone-900 border border-sky-500 inline-block rounded-xl shadow-2xl font-bold hover:bg-gradient-to-br"
-          href={link}
-          target="_blank"
-          rel="noopener noreferrer">
-          Ver repósitório
-        </a>
-      )}
+      <div className="w-full grid grid-cols-2 gap-4">
+        {link && (
+          <a
+            className="grid-cols-1 px-4 py-2 h-12 bg-stone-900 border border-black flex items-center justify-center rounded-md shadow-2xl font-bold hover:bg-stone-800 transition-all"
+            href={link}
+            target="_blank"
+            rel="noopener noreferrer">
+            Repósitório
+          </a>
+        )}
+
+        {preview && (
+          <a
+            className="grid-cols-1 px-4 py-2 h-12 bg-stone-900 border border-black flex items-center justify-center rounded-md shadow-2xl font-bold hover:bg-stone-800 transition-all"
+            href={preview}
+            target="_blank"
+            rel="noopener noreferrer">
+            Preview
+          </a>
+        )}
+      </div>
     </div>
   );
 };
--- src/components/Sections/ProjectsSection/index.tsx
+++ src/components/Sections/ProjectsSection/index.tsx
@@ -3,6 +3,76 @@ import ProjectCard from "../../ProjectCard";
 import TitleSection from "../../TitleSection";
 
 const ProjectsSection = () => {
+
+  const projects = [
+    {
+      title: "Marvel Next App",
+      image: "../imgs/marvel.png",
+      text: "Site feito com Next.JS para exibir informações sobre os personagens, quadrinhos e outros dados.",
+      tec: "Next.Js, Typescript, React Query e Tailwind",
+      link: "https://github.com/CaioSouzaC1/ntf",
+      badge: "Frontend",
+      preview: "https://marvel.caiosouza.dev/",
+    },
+    {
+      title: "Futsal Web",
+      image: "../imgs/futsal.png",
+      text: "Aplicativo feito com Next.JS para consumir a api de futsal feita em Laravel.",
+      tec: "Next.Js, Typescript e Tailwind",
+      link: "https://github.com/CaioSouzaC1/futsal-web",
+      badge: "Frontend",
+    },
+    {
+      title: "Futsal Api Laravel",
+      image: "../imgs/API.png",
+      text: "Api de um sistema para gerenciar informações sobre jogadores, times e partidas de futsal.",
+      tec: "Laravel",
+      link: "https://github.com/CaioSouzaC1/futsal-api-laravel",
+      badge: "Backend",
+    },
+    {
+      title: "Futsal Api Node.js",
+      image: "../imgs/API.png",
+      text: "Api de um sistema para gerenciar informações sobre jogadores, times e partidas de futsal.",
+      tec: "Node.js, Typescript, Prisma & Express",
+      link: "https://github.com/CaioSouzaC1/futsal-api",
+      badge: "Backend",
+    },
+    {
+      title: "Notizia (Tema Wordpress)",
+      image: "../imgs/Notizia.png",
+      text: "Site que gera recomendações de animes a partir de um algoritomo que se baseia em obras que você já assistiu.",
+      tec: "Wordpress, Javascript & Tailwind",
+      badge: "Fullstack",
+    },
+    {
+      title: "Anime Match",
+      image: "../imgs/Anime.png",
+      text: "Site que gera recomendações de animes a partir de um algoritomo que se baseia em obras que você já assistiu.",
+      tec: "React, Javascript & Tailwind",
+      link: "https://github.com/CaioSouzaC1/Anime-Match",
+      badge: "Frontend",
+      preview: "https://anime-match.vercel.app/",
+    },
+    {
+      title: "React Pokedex",
+      image: "../imgs/Pokedex.png",
+      text: "Uma Pokedex, com funcionalidades de trocar de estilo das artes, seleção por geração e filtro.",
+      tec: "React, Javascript & Tailwind",
+      link: "https://github.com/CaioSouzaC1/ReactPokedex3",
+      badge: "Frontend",
+      preview: "https://benevolent-dolphin-4afc2a.netlify.app/",
+    },
+    {
+      title: "Voice",
+      image: "../imgs/Voice.png",
+      text: "Plataforma de comunicação entre artistas e dono de estabelecimentos, com chat integrado e marcação de eventos.",
+      tec: "React, Firebase & Tailwind",
+      link: "https://github.com/CaioSouzaC1/TccEtec2023",
+      badge: "Fullstack",
+    },
+  ];
+
   return (
     <section
       id="projects"
@@ -10,47 +80,9 @@ const ProjectsSection = () => {
       <Container classNames="flex flex-wrap justify-around">
         <TitleSection title="Projetos" />
         <div className="w-full flex flex-wrap">
-          <ProjectCard
-            title="Futsal Api Laravel"
-            image="../imgs/API.png"
-            text="Api de um sistema para gerenciar informações sobre jogadores, times e partidas de futsal."
-            tec="Laravel"
-            link="https://github.com/CaioSouzaC1/futsal-api-laravel"
-          />
-          <ProjectCard
-            title="Futsal Api Node.js"
-            image="../imgs/API.png"
-            text="Api de um sistema para gerenciar informações sobre jogadores, times e partidas de futsal."
-            tec="Node.js, Typescript, Prisma & Express"
-            link="https://github.com/CaioSouzaC1/futsal-api"
-          />
-          <ProjectCard
-            title="Notizia (Tema Wordpress)"
-            image="../imgs/Notizia.png"
-            text="Site que gera recomendações de animes a partir de um algoritomo que se baseia em obras que você já assistiu."
-            tec="Wordpress, Javascript & Tailwind css"
-          />
-          <ProjectCard
-            title="Anime Match"
-            image="../imgs/Anime.png"
-            text="Site que gera recomendações de animes a partir de um algoritomo que se baseia em obras que você já assistiu."
-            tec="React, Javascript & Tailwind css"
-            link="https://github.com/CaioSouzaC1/Anime-Match"
-          />
-          <ProjectCard
-            title="React Pokedex"
-            image="../imgs/Pokedex.png"
-            text="Uma Pokedex, com funcionalidades de trocar de estilo das artes, seleção por geração e filtro."
-            tec="React, Javascript & Tailwind css"
-            link="https://github.com/CaioSouzaC1/ReactPokedex3"
-          />
-          <ProjectCard
-            title="Voice"
-            image="../imgs/Voice.png"
-            text="Plataforma de comunicação entre artistas e dono de estabelecimentos, com chat integrado e marcação de eventos."
-            tec="React, Firebase & Tailwind css"
-            link="https://github.com/CaioSouzaC1/TccEtec2023"
-          />
+          {projects.map((e, i) => (
+            <ProjectCard key={i} info={e} />
+          ))}
         </div>
       </Container>
     </section>
